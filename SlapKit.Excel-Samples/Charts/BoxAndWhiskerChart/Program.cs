1a7d3a62fadc9ced7b38e05a6eeb39c79d272eb6
SlapKit Admin <admin@slapkit.com>
2024-11-01 13:34:43 +0200

    Upgrade the Box and Whisker chart sample

--- SlapKit.Excel-Samples/Charts/BoxAndWhiskerChart/Program.cs
+++ SlapKit.Excel-Samples/Charts/BoxAndWhiskerChart/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Helpers;
 using SlapKit.Excel.Excel;
+using SlapKit.Excel.Excel.Charts.ExtendedChartTypeFeatures;
+using SlapKit.Excel.Excel.Charts.ExtendedSeriesFeatures;
 
 var workbook = new XLWorkbook();
 var worksheet = workbook.AddWorksheet();
@@ -9,21 +11,56 @@ DataGenerator.GenerateSchoolCoursesData(worksheet);
 var chart = worksheet.Charts.AddBoxAndWhiskerChart();
 chart.MoveTo(worksheet.Cell("F2"), worksheet.Cell("N18"));
 
+chart.CategoryAxis.GapWidthRatio = 0.24;
+chart.CategoryAxis.MajorTickMark = XLAxisTickMark.Inside;
+
+chart.ValueAxis.MinorTickMark = XLAxisTickMark.Inside;
+chart.ValueAxis.MajorTickMark = XLAxisTickMark.Outside;
+chart.ValueAxis.Scaling.MinAxisValue = 45;
+chart.ValueAxis.SetDisplayUnit(x =>
+{
+    x.Type = XLAxisDisplayUnitType.Hundreds;
+    x.SetLabel();
+});
+
 var categories = worksheet.Range("A2:A16");
 
 chart.Series.Add()
     .SetName(worksheet.Range("B1"))
     .SetCategories(categories)
-    .SetValues(worksheet.Range("B2:B16"));
+    .SetValues(worksheet.Range("B2:B16"))
+    .SetElementVisibilities(x =>
+    {
+        x.ShowMeanLine = false;
+        x.ShowMeanMarkers = false;
+        x.ShowInnerPoints = false;
+        x.ShowOutlierPoints = false;
+    });
 
 chart.Series.Add()
     .SetName(worksheet.Range("C1"))
     .SetCategories(categories)
-    .SetValues(worksheet.Range("C2:C16"));
+    .SetValues(worksheet.Range("C2:C16"))
+    .SetQuartileMethod(XLSeriesQuartileMethod.Exclusive)
+    .SetElementVisibilities(x =>
+    {
+        x.ShowMeanLine = true;
+        x.ShowMeanMarkers = true;
+        x.ShowInnerPoints = true;
+        x.ShowOutlierPoints = true;
+    });
 
 chart.Series.Add()
     .SetName(worksheet.Range("D1"))
     .SetCategories(categories)
-    .SetValues(worksheet.Range("D2:D16"));
+    .SetValues(worksheet.Range("D2:D16"))
+    .SetQuartileMethod(XLSeriesQuartileMethod.Inclusive)
+    .SetElementVisibilities(x =>
+    {
+        x.ShowMeanLine = false;
+        x.ShowMeanMarkers = true;
+        x.ShowInnerPoints = false;
+        x.ShowOutlierPoints = false;
+    });
 
 workbook.SaveAs("workbook.xlsx");
--- SlapKit.Excel-Samples/Charts/BoxAndWhiskerChart/README.md
+++ SlapKit.Excel-Samples/Charts/BoxAndWhiskerChart/README.md
@@ -3,6 +3,13 @@
 This sample demonstrates how to:
 - create Box and Whisker chart with multiple series
 - position the chart using 2 cell anchor ("Move and size with cells" property in Excel)
+- configure series' Gap Width
+- configure category axis' major tick marks
+- configure value axis' major and minor tick marks
+- configure value axis' minimum axis value ("Bounds > Minimum" in Excel)
+- configure value axis' "Display units" 
+- configure series' Quartile calculation method
+- configure series' "Show mean line", "Show mean markers", "Show inner points", "Show outlier points" options
 
 ## Output
 
